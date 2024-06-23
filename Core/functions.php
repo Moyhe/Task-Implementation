55b2691bb4fa7326e8cb17d7929d9d0c8b0beb84
Mohye <dragonsinmeliodas777@gmail.com>
2024-06-24 00:37:45 +0300

    add helper functions

--- Core/functions.php
+++ Core/functions.php
@@ -4,3 +4,11 @@ function base_path($path)
 {
     return BASE_PATH . $path;
 }
+
+
+function view($path, $attributes = [])
+{
+    extract($attributes);
+
+    require base_path('views/' . $path);
+}
